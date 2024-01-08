5f8ebbebb722a94b369ff505222e5e69d08f401c
Trần Trọng Hiến <trantronghien85@gmail.com>
2024-01-08 22:54:31 +0700

    fix middle admin

--- src/middlewares/api-response-middleware.js
+++ src/middlewares/api-response-middleware.js
@@ -1,13 +1,22 @@
 module.exports = (config, { strapi }) => {
     return async (context, next) => {
-        await next();
-        console.log(context.response);
-        let body = null;
-        if (context.response.body) body = context.response.body;
-        return context.send({
-            message: context.message,
-            status: context.status,
-            data: body,
-        })
+        const { url } = context.request;
+        const prefixAdmin = /^\/api\//;
+        if (prefixAdmin.test(url)) {
+            // continue if != /admin
+            await next();
+            // console.log(context.response);
+            let body = null;
+            if (context.response.body) body = context.response.body;
+            return context.send({
+                message: context.message,
+                status: context.status,
+                data: body,
+            })
+        } else {
+            return await next();
+        }
+
+
     };
 };
